a172f8e7182d99967677df65d609aa91dcf68d65
SARAB0297 <297sarab.work@gmail.com>
2024-08-30 02:35:11 +0530

    page3_completed

--- script.js
+++ script.js
@@ -66,6 +66,29 @@ function page2Animation() {
     })
 }
 
+function page3VideoAnimation() {
+var page3Center=document.querySelector(".page3-center");
+var video=document.querySelector("#page3 video");
+
+page3Center.addEventListener("click",function(){
+
+    video.play()
+    gsap.to(video, {
+        transform:'scaleX(1) scaleY(1)',
+        opacity:1,
+        borderRadius:0 
+    })
+})
+video.addEventListener("click",function(){
+    video.pause()
+    gsap.to(video, {
+        transform:'scaleX(0.7) scaleY(0)',
+        opacity:0,
+        borderRadius:"30px"
+    })
+})
+}
 
 navAnimation();
 page2Animation();
+page3VideoAnimation();
